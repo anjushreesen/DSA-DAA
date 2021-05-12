5ef9846f9ec02a6a84edadc5813b0672de5e07d3
Amitrajit31 <amitrajitdas31@gmail.com>
2021-05-13 02:12:57 +0530

    refactored

--- Prims.cpp
+++ Prims.cpp
@@ -2,7 +2,8 @@
 
 #include <iostream>
 #define I INT_MAX
-    using namespace std;
+using namespace std;
+
 int cost[][8] =
     {{I, I, I, I, I, I, I, I},
      {I, I, 25, I, I, I, 5, I},
@@ -12,6 +13,7 @@ int cost[][8] =
      {I, I, I, I, 16, I, 20, 18},
      {I, 5, I, I, I, 20, I, I},
      {I, I, 10, I, 14, 18, I, I}};
+
 int near[8] = {I, I, I, I, I, I, I, I};
 int t[2][6];
 int main()
